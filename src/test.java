67eb07c001018d8a4e652881e1fadd0a7a7964e4
12Dong <289663639@qq.com>
2018-04-18 22:59:27 +0800

    偷懒版

--- src/test.java
+++ src/test.java
@@ -1,7 +1,8 @@
 public class test {
     static public void main(String argv[]){
-        String regx = "^[a-zA-Z]{1}[a-zA-Z0-9]*";
-        String num = "14213123123";
-        System.out.println(num.matches(regx));
+        String segments[] = "read(x);".split("[()]");
+        for(String segment:segments){
+            System.out.println(segment);
+        }
     }
 }
--- src/week3.java
+++ src/week3.java
@@ -1,6 +1,8 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
 
 public class week3 {
     static Map<String,String> map;
@@ -60,10 +62,99 @@ public class week3 {
                     continue;
                 }
 
+                if(str.contains("(")&&str.contains(")")){
+                    String segments[] = str.split("[()]");
+                    if(map.containsKey(segments[0])){
+                        System.out.println("("+map.get(segments[0])+"   "+segments[0]+")");
+                        System.out.println("("+signMap.get("(")+"    "+"()");
+                        if(judgeVarStack(segments[1])){
+                            System.out.println("(ident    "+segments[1]+")");
+                            System.out.println("("+signMap.get(")")+"    ))");
+                            System.out.println("("+signMap.get(";")+"    ;)");
+                        }else{
+                            //变量未出现 在 变量池中
+                            System.out.println(rows+" 行 非法变量 未定义");
+                            return ;
+                        }
+
+                    }else{
+                        //左括号前的 关键词 非法
+                        System.out.println(rows+" 行 非法关键字");
+                        return ;
+                    }
+                    continue;
+                }else if(str.contains("(")  || str.contains(")")){
+                    System.out.println(rows+" 行 缺少 （ 或者 ）");
+                    return ;
+                }
                 filter = "";
                 //对应 一行 多段 异端 编码 世界上 怎么会有如此naocan的编码方式
                 String segments[] = str.split(";");
                 for (String segment : segments) {
+
+                    if(segment.contains(":=")){
+                        String words[] = segment.split(":=");
+
+                        // words[0] 目标对象
+                        // words[1] 表达式
+                        Stack<Integer> tmpVarStack = new Stack<>();
+//                        检测 目标对象1 是否存在
+                        if(constStack.containsKey(words[0])){
+                            System.out.println(rows+" 行 编译错误 常量 无法 改变值");
+                            return ;
+                        }
+                        if(functionStack.containsKey(words[0])){
+                            System.out.println(rows+" 行 编译错误 函数 无法 被改变");
+                            return ;
+                        }
+                        if(varStack.containsKey(words[0])==false){
+                            System.out.println(rows+" 行 编译错误 变量未被定义");
+                            return;
+                        }
+                        System.out.println("(ident    "+words[0]+")");
+                        System.out.println("("+signMap.get(":=")+"    :=)");
+                        String var = "";
+                        for(int i=0;i<words[1].length();i++){
+                            //遇到 符号
+                            String current = ""+words[1].charAt(i);
+                            if(signMap.containsKey(current)){
+                                if(functionStack.containsKey(var)){
+                                    System.out.println(rows+" 行 编译错误 函数无法参与运算");
+                                    return;
+                                }
+                                if(constStack.containsKey(var)){
+                                    tmpVarStack.push(constStack.get(var));
+                                    System.out.println("(ident     "+var+")");
+                                }else if(varStack.containsKey(var)){
+                                    tmpVarStack.push(varStack.get(var));
+                                    System.out.println("(ident     "+var+")");
+
+                                }else{
+                                    System.out.println(rows+" 行 编译错误 变量未被定义");
+                                    return ;
+                                }
+                                System.out.println("("+signMap.get(current)+"    "+current+")");
+                                var="";
+                            }else{
+                                var+=words[1].charAt(i);
+                            }
+                        }
+                        if(functionStack.containsKey(var)){
+                            System.out.println(rows+" 行 编译错误 函数无法参与运算");
+                            return;
+                        }
+                        if(constStack.containsKey(var)){
+                            tmpVarStack.push(constStack.get(var));
+                            System.out.println("(ident     "+var+")");
+                        }else if(varStack.containsKey(var)){
+                            tmpVarStack.push(varStack.get(var));
+                            System.out.println("(ident     "+var+")");
+
+                        }else{
+                            System.out.println(rows+" 行 编译错误 变量未被定义");
+                            return ;
+                        }
+                    }
                     //分割 空格 转化为 Type  expression格式 如果 有的话
                     String words[] = segment.split(" ");
                     if (map.containsKey(words[0])) {
@@ -74,7 +165,12 @@ public class week3 {
                             System.out.println("(" + map.get("const") + "   " + "const)");
                             //逗号 分割 表达式
                             String expressions[] = words[1].split(",");
+                            boolean onlyOnce = false;
                             for (String expression : expressions) {
+//                                逗号 表达
+                                if(onlyOnce==true){
+                                    System.out.println("("+signMap.get(",")+"   ,)");
+                                }
                                 //常量 必须在初始化时 被赋值 也就是 每个 expression 必须有 =
                                 if (expression.contains("=")) {
 //                                    分割为 var =  value
@@ -105,16 +201,21 @@ public class week3 {
                                 } else {
                                     System.out.println(rows + "   行 编译错误 常量必须在初始化时 被赋值");
                                 }
+                                onlyOnce = true;
                             }
                         }
 
 //                        ===================================================================
                         //  变量处理方法
-                        if(words[0].equals("var")){
+                        else if(words[0].equals("var")){
                             System.out.println("(" + map.get("var") + "   " + "var)");
                             //逗号 分割 表达式
                             String expressions[] = words[1].split(",");
+                            boolean onlyOnce = false;
                             for (String expression : expressions) {
+                                if(onlyOnce==true){
+                                    System.out.println("("+signMap.get(",")+"    ,)");
+                                }
                                 // 变量 可以初始化 也可以不初始化
                                 if(expression.contains("=")){
                                     String process[] = expression.split("=");
@@ -122,11 +223,32 @@ public class week3 {
                                         System.out.println(rows+" 行  = 右边 没有 赋值");
                                         return ;
                                     }else{
+
+
                                         String regex = "^[0-9]*";
                                         //判断 value 合法
                                         if (process[1].matches(regex) == false) {
-                                            System.out.println(rows + " 行 var变量 应为整数");
-                                            return;
+                                            //判断等式 右边 是否 为 变量 or 常量
+                                            if(varStack.containsKey(process[1]) || constStack.containsKey(process[1])){
+                                                //判断 var 重名
+                                                if (judgeExit(process[0]) == false) {
+                                                    System.out.println(rows + " 行 var变量名 已被使用");
+                                                    return;
+                                                }
+                                                if (judgeLegal(process[0]) == false) {
+                                                    System.out.println(rows + " 行 var变量名 命名非法");
+                                                    return;
+                                                }
+                                                System.out.println("(ident    " + process[0] + ")");
+                                                System.out.println("(eql      =)");
+                                                System.out.println("(ident    " + process[1] + ")");
+                                                if(varStack.containsKey(process[1])) varStack.put(process[0],varStack.get(process[1]));
+                                                if(constStack.containsKey(process[1])) varStack.put(process[0],constStack.get(process[1]));
+                                            }
+                                            else{
+                                                System.out.println(rows + " 行 var变量 应为整数 或 变量");
+                                                return ;
+                                            }
                                         } else {
                                             //判断 var 重名
                                             if (judgeExit(process[0]) == false) {
@@ -153,17 +275,40 @@ public class week3 {
                                         System.out.println(rows + " 行 var变量名 命令非法");
                                     }
                                     System.out.println("(ident    " + expression + ")");
-                                    System.out.println("(eql      =)");
-                                    System.out.println("(number   " + expression + ")");
-                                    varStack.put(expression, Integer.MIN_VALUE);
+                                    varStack.put(expression, 0);
                                 }
-
+                                onlyOnce = true;
+                            }
+                        }
+                        else if(words[0].equals("procedure")){
+                            // 处理 函数
+                            if(judgeExit(words[1])==false){
+                                System.out.println(rows+" 行 编译错误 函数名被占用");
+                                return;
+                            }else{
+                                System.out.println("("+map.get("procedure")+"    procedure)");
+                                functionStack.put(words[1],rows);
+                                System.out.println("(ident     "+words[1]+")");
+                            }
+                        }
+                        else if(words[0].equals("call")){
+                            // 处理 函数
+                            if(judgeExit(words[1])==true){
+                                System.out.println(rows+" 行 编译错误 函数名未定义");
+                                return;
+                            }else{
+                                System.out.println("("+map.get("call")+"    call)");
+                                System.out.println("(ident     "+words[1]+")");
                             }
                         }
 
 //                        非 变量 常量 定义
-                        System.out.println("("+map.get(words[0])+"     "+words[0]+")");
+                        else {
+                            System.out.println("("+map.get(words[0])+"     "+words[0]+")");
+                        }
                     }
+
+
                     //输出 为 split 分割 的 ;
                     System.out.println("(semicolon  ;)");
                 }
@@ -189,10 +334,12 @@ public class week3 {
             map.put("while","whilesym");
             map.put("begin","beginsym");
             map.put("read","readsym");
+            map.put("write","writesym");
             map.put("end","endsym");
         }
         public static void makeSignMap(){
             signMap = new HashMap<>();
+            signMap.put(":=","becomes");
             signMap.put("=","eql");
             signMap.put(";","semicolon");
             signMap.put(",","comma");
@@ -221,5 +368,8 @@ public class week3 {
             String regex = "^[a-zA-Z]{1}[a-zA-Z0-9]*";
             return var.matches(regex);
 
+        }
+        static boolean judgeVarStack(String varName){
+            return varStack.containsKey(varName);
         }
 }
